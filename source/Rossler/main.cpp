872de27e8973c2f03fdf430f336e6894e4d6022d
SaveliyKrivov <krivovsaveliy28@gmail.com>
2024-06-09 11:22:10 +0300

    Final_3

--- source/Rossler/main.cpp
+++ source/Rossler/main.cpp
@@ -4,75 +4,60 @@
 
 #include <iostream>
 #include <cmath>
-#include <vector>
 #include <fstream>
 #include <iomanip>
 
 // параметры модели ////////////////////////////////////
-const double t0 = 0.0;      // временной 
+const double t0 = 0.0;      // временной
 const double T = 1000.0;    // интервал
 const double a = 0.2;
 const double b = 0.2;
 const double c = 5.7;
 /////////////////////////////////////////////////////////
 
-// правые части системы ОДУ 
+struct dbl3
+{
+    double x;
+    double y;
+    double z;
+};
+
+// правые части системы ОДУ
 double f_x( double, double, double, double );
 double f_y( double, double, double, double );
 double f_z( double, double, double, double );
 
 // метод Рунге-Кутты 4-го порядка
-std::vector<double>
-Runge_Kutta( double(*f_x)( double, double, double, double ), 
-	         double(*f_y)( double, double, double, double ), 
-	         double(*f_z)( double, double, double, double ),
-	         const double&, const double&, const double&,
-	         const double&, const double&  );
+dbl3 Runge_Kutta( double(*f_x)( double, double, double, double ),
+                  double(*f_y)( double, double, double, double ),
+                  double(*f_z)( double, double, double, double ),
+                  const double&, const double&, const double&,
+                  const double&, const double&  );
 
 
-int main( int argc, char** argv ) 
+int main( int argc, char** argv )
 {
-    
- // количество точек 
- int n = 15001;
- double dt = (T - t0) / (n - 1);
- 
- std::vector<double> x( n, 0 );
- std::vector<double> y( n, 0 );
- std::vector<double> z( n, 0 );
- 
- // начальные условия
- x[0] = 0.0;
- y[0] = 0.0;
- z[0] = 0.0;
- 
- for ( size_t i = 1; i < n; i++ )
- {
-	 std::vector<double> 
-	 p = Runge_Kutta( f_x, f_y, f_z, dt, t0 + dt*(i - 1), 
-		              x[i - 1], y[i - 1], z[i - 1] );
-	 x[i] = p[0];
-	 y[i] = p[1];
-	 z[i] = p[2];
- }
- 
- std::ofstream  data_file( "Rossler.d" );   // файл с числ. данными;
- std::cout.precision(8); 
-// std::cout << " t    " << "x             " << "y             " << "z\n";
- for ( size_t i = 0; i < n; i++ )
- {
-	 double t = t0 + i*dt;
-//	 std::cout << " " << std::setw(12) << std::left << t  
-//	           << std::setw(14) << x[i] 
-//			   << std::setw(14) << y[i] 
-//			   << std::setw(14) << z[i] << "\n";
-	 data_file << " " << std::setw(12) << std::left << t 
-	           << std::setw(16) << x[i] 
-	           << std::setw(16) << y[i] 
-	           << std::setw(16) << z[i] << "\n";		   
- }
- data_file.close();
- return 0;
+    // количество точек
+    int n = 15001;
+    double dt = (T - t0) / (n - 1);
+
+    // начальные условия
+    dbl3 p = { 0.0, 0.0, 0.0 };
+    std::ofstream  data_file( "Rossler.d" ); // файл с числ. данными;
+    std::cout.precision(8);
+
+    for ( size_t i = 0; i < n; i++ )
+    {
+        data_file << " " << std::setw(12) << std::left << t0 + i*dt
+                  << std::setw(16) << p.x
+                  << std::setw(16) << p.y
+                  << std::setw(16) << p.z << "\n";
+
+        p = Runge_Kutta(f_x, f_y, f_z, dt, t0 + dt*(i-1), p.x, p.y, p.z);
+    }
+    std::cout << "Data file: Rossler.d\n";
+    data_file.close();
+    return 0;
 }
 
 //-------------------------------------------------------------------
@@ -82,57 +67,56 @@ int main( int argc, char** argv )
 // z' = f_z
 // v' = f_v
 //-------------------------------------------------------------------
-std::vector<double>
-Runge_Kutta( double(*f_x)( double, double, double, double ), 
-	         double(*f_y)( double, double, double, double ), 
-	         double(*f_z)( double, double, double, double ),
-	         const double &dt, 
-	         const double &t, 
-	         const double &x,
-	         const double &y,
-	         const double &z  )
+dbl3 Runge_Kutta( double(*f_x)( double, double, double, double ),
+                  double(*f_y)( double, double, double, double ),
+                  double(*f_z)( double, double, double, double ),
+                  const double &dt,
+                  const double &t,
+                  const double &x,
+                  const double &y,
+                  const double &z  )
 {
-	double	
-	k11 = dt * f_x( t, x, y, z ),
-	k12 = dt * f_y( t, x, y, z ), 
-	k13 = dt * f_z( t, x, y, z ), 
-	
-	k21 = dt * f_x( t + dt/2, x + k11/2, y + k12/2, z + k13/2 ),
-	k22 = dt * f_y( t + dt/2, x + k11/2, y + k12/2, z + k13/2 ),
-	k23 = dt * f_z( t + dt/2, x + k11/2, y + k12/2, z + k13/2 ),
-	
-	k31 = dt * f_x( t + dt/2, x + k21/2, y + k22/2, z + k13/2 ),
-	k32 = dt * f_y( t + dt/2, x + k21/2, y + k22/2, z + k13/2 ),
-	k33 = dt * f_z( t + dt/2, x + k21/2, y + k22/2, z + k13/2 ),
-	
-	k41 = dt * f_x( t + dt,   x + k31,   y + k32, z + k33   ),
-	k42 = dt * f_y( t + dt,   x + k31,   y + k32, z + k33   ),
-	k43 = dt * f_z( t + dt,   x + k31,   y + k32, z + k33   );
-	
-	return { x + (k11 + 2*k21 + 2*k31 + k41) / 6, 
-		     y + (k12 + 2*k22 + 2*k32 + k42) / 6,
-		     z + (k13 + 2*k23 + 2*k33 + k43) / 6  };
+    double
+            k11 = dt * f_x( t, x, y, z ),
+            k12 = dt * f_y( t, x, y, z ),
+            k13 = dt * f_z( t, x, y, z ),
+
+            k21 = dt * f_x( t + dt/2, x + k11/2, y + k12/2, z + k13/2 ),
+            k22 = dt * f_y( t + dt/2, x + k11/2, y + k12/2, z + k13/2 ),
+            k23 = dt * f_z( t + dt/2, x + k11/2, y + k12/2, z + k13/2 ),
+
+            k31 = dt * f_x( t + dt/2, x + k21/2, y + k22/2, z + k23/2 ),
+            k32 = dt * f_y( t + dt/2, x + k21/2, y + k22/2, z + k23/2 ),
+            k33 = dt * f_z( t + dt/2, x + k21/2, y + k22/2, z + k23/2 ),
+
+            k41 = dt * f_x( t + dt,   x + k31,   y + k32,   z + k33   ),
+            k42 = dt * f_y( t + dt,   x + k31,   y + k32,   z + k33   ),
+            k43 = dt * f_z( t + dt,   x + k31,   y + k32,   z + k33   );
+
+    return { x + (k11 + 2*k21 + 2*k31 + k41) / 6,
+             y + (k12 + 2*k22 + 2*k32 + k42) / 6,
+             z + (k13 + 2*k23 + 2*k33 + k43) / 6  };
 }
 //-------------------------------------------------------------------
 // правая часть уравнения x' = f_x( t, x, y, z ) 
 //-------------------------------------------------------------------
 double f_x( double t, double x, double y, double z )
 {
-	return -y - z; 
+    return -y - z;
 }
 //-------------------------------------------------------------------
 // правая часть уравнения y' = f_y( t, x, y, z ) 
 //-------------------------------------------------------------------
 double f_y( double t, double x, double y, double z )
 {
-	return  x + a*y; 
+    return  x + a*y;
 }
 //-------------------------------------------------------------------
 // правая часть уравнения z' = f_z( t, x, y, z ) 
 //-------------------------------------------------------------------
 double f_z( double t, double x, double y, double z )
 {
-	return b + z*(x - c); 
+    return b + z*(x - c);
 }
 
 
--- source/Rossler_a/main.cpp
+++ source/Rossler_a/main.cpp
@@ -5,81 +5,77 @@
 
 #include <iostream>
 #include <cmath>
-#include <vector>
 #include <fstream>
 #include <iomanip>
 
 // параметры модели ////////////////////////////////////
-const double t0 = 0.0;      // временной 
+const double t0 = 0.0;      // временной
 const double T = 100.0;     // интервал
 double a = 0.0;
 const double b = 0.2;
 const double c = 5.7;
 /////////////////////////////////////////////////////////
 
-// правые части системы ОДУ 
+struct dbl3
+{
+    double x;
+    double y;
+    double z;
+};
+
+// правые части системы ОДУ
 double f_x( double, double, double, double );
 double f_y( double, double, double, double );
 double f_z( double, double, double, double );
 
 // метод Рунге-Кутты 4-го порядка
-std::vector<double>
-Runge_Kutta( double(*f_x)( double, double, double, double ), 
-	double(*f_y)( double, double, double, double ), 
-	double(*f_z)( double, double, double, double ),
-	const double&, const double&, const double&,
-	const double&, const double&  );
+dbl3 Runge_Kutta( double(*f_x)( double, double, double, double ),
+                  double(*f_y)( double, double, double, double ),
+                  double(*f_z)( double, double, double, double ),
+                  const double&, const double&, const double&,
+                  const double&, const double&  );
 
-
-int main( int argc, char** argv ) 
+//-------------------------------------------------------------------
+int main( int argc, char** argv )
 {
-	
-	// количество точек 
-	int n = 5001;
-	double dt = (T - t0) / (n - 1);
-	
-	std::vector<double> x( n, 0 );
-	std::vector<double> y( n, 0 );
-	std::vector<double> z( n, 0 );
-	
-	// границы изменения параметра b
-	const double a_min = -4.0;
-	const double a_max = 4.0;
-	int m = 9001;
-	double ac = (a_max - a_min) / (m - 1);
-	
-	std::ofstream  data_file( "Rossler_a.d" ); // файл с числ. данными;
-	std::cout.precision(8); 
-	
-	a = a_min;
-	for ( size_t k = 0;  k < m; ++k  )
-	{
-		// начальные условия
-		x[0] = 1.0;
-		y[0] = 1.0;
-		z[0] = 0.1;
-		for ( size_t i = 1; i < n; i++ )
-		{
-			std::vector<double> 
-			p = Runge_Kutta( f_x, f_y, f_z, dt, t0 + dt*(i - 1), 
-				x[i - 1], y[i - 1], z[i - 1] );
-			x[i] = p[0];
-			y[i] = p[1];
-			z[i] = p[2];
-		}
-		//double max_x = -1.0e16; 
-		for ( size_t i = 1; i < n - 1; i++ )
-		{ 
-			// локальные максимумы x
-			if ( ( x[i - 1] < x[i] ) && (x[i] > x[i + 1]) ) 
-				data_file << std::setw(16) << a << std::setw(16) << x[i] << "\n";	;	
-		}	
-		
-		a += ac;
-	}
-	std::cout << "Data file: Rossler_a.d\n";		   
-	data_file.close();
-	return 0;
+
+    // количество точек
+    int n = 5001;
+    double dt = (T - t0) / (n - 1);
+
+    // границы изменения параметра a
+    const double a_min = -2.0;
+    const double a_max = 2.0;
+    int m = 9001;
+    double ac = (a_max - a_min) / (m - 1);
+
+    std::ofstream  data_file( "Rossler_a.d" ); // файл с числ. данными;
+    std::cout.precision(8);
+
+    a = a_min;
+
+    for ( size_t k = 0;  k < m; ++k  )
+    {
+        // начальные условия
+        dbl3 p_l = { 1.0, 1.0, 0.1 };
+        dbl3 p_c = Runge_Kutta( f_x, f_y, f_z, dt, t0 + dt,
+                                p_l.x, p_l.y, p_l.z );
+
+        for ( size_t i = 1; i < n - 1;  ++i )
+        {
+            dbl3 p_r = Runge_Kutta( f_x, f_y, f_z, dt, t0 + dt*(i + 1),
+                                    p_c.x, p_c.y, p_c.z );
+            // локальные максимумы x
+            if ( ( p_l.x < p_c.x ) && (p_c.x > p_r.x ) && (fabs(p_c.x) < 100 ))
+                data_file << std::setw(16) << a << std::setw(16) << p_c.x << "\n";
+            p_l = p_c;
+            p_c = p_r;
+        }
+        a += ac;
+    }
+    std::cout << "Data file: Rossler_a.d\n";
+    data_file.close();
+    return 0;
 }
 
 //-------------------------------------------------------------------
@@ -89,56 +85,55 @@ int main( int argc, char** argv )
 // z' = f_z
 // v' = f_v
 //-------------------------------------------------------------------
-std::vector<double>
-Runge_Kutta( double(*f_x)( double, double, double, double ), 
-	double(*f_y)( double, double, double, double ), 
-	double(*f_z)( double, double, double, double ),
-	const double &dt, 
-	const double &t, 
-	const double &x,
-	const double &y,
-	const double &z  )
+dbl3 Runge_Kutta( double(*f_x)( double, double, double, double ),
+                  double(*f_y)( double, double, double, double ),
+                  double(*f_z)( double, double, double, double ),
+                  const double &dt,
+                  const double &t,
+                  const double &x,
+                  const double &y,
+                  const double &z  )
 {
-	double	
-	k11 = dt * f_x( t, x, y, z ),
-	k12 = dt * f_y( t, x, y, z ), 
-	k13 = dt * f_z( t, x, y, z ), 
-	
-	k21 = dt * f_x( t + dt/2, x + k11/2, y + k12/2, z + k13/2 ),
-	k22 = dt * f_y( t + dt/2, x + k11/2, y + k12/2, z + k13/2 ),
-	k23 = dt * f_z( t + dt/2, x + k11/2, y + k12/2, z + k13/2 ),
-	
-	k31 = dt * f_x( t + dt/2, x + k21/2, y + k22/2, z + k13/2 ),
-	k32 = dt * f_y( t + dt/2, x + k21/2, y + k22/2, z + k13/2 ),
-	k33 = dt * f_z( t + dt/2, x + k21/2, y + k22/2, z + k13/2 ),
-	
-	k41 = dt * f_x( t + dt,   x + k31,   y + k32, z + k33   ),
-	k42 = dt * f_y( t + dt,   x + k31,   y + k32, z + k33   ),
-	k43 = dt * f_z( t + dt,   x + k31,   y + k32, z + k33   );
-	
-	return { x + (k11 + 2*k21 + 2*k31 + k41) / 6, 
-		y + (k12 + 2*k22 + 2*k32 + k42) / 6,
-		z + (k13 + 2*k23 + 2*k33 + k43) / 6  };
+    double
+            k11 = dt * f_x( t, x, y, z ),
+            k12 = dt * f_y( t, x, y, z ),
+            k13 = dt * f_z( t, x, y, z ),
+
+            k21 = dt * f_x( t + dt/2, x + k11/2, y + k12/2, z + k13/2 ),
+            k22 = dt * f_y( t + dt/2, x + k11/2, y + k12/2, z + k13/2 ),
+            k23 = dt * f_z( t + dt/2, x + k11/2, y + k12/2, z + k13/2 ),
+
+            k31 = dt * f_x( t + dt/2, x + k21/2, y + k22/2, z + k23/2 ),
+            k32 = dt * f_y( t + dt/2, x + k21/2, y + k22/2, z + k23/2 ),
+            k33 = dt * f_z( t + dt/2, x + k21/2, y + k22/2, z + k23/2 ),
+
+            k41 = dt * f_x( t + dt,   x + k31,   y + k32, z + k33   ),
+            k42 = dt * f_y( t + dt,   x + k31,   y + k32, z + k33   ),
+            k43 = dt * f_z( t + dt,   x + k31,   y + k32, z + k33   );
+
+    return { x + (k11 + 2*k21 + 2*k31 + k41) / 6,
+             y + (k12 + 2*k22 + 2*k32 + k42) / 6,
+             z + (k13 + 2*k23 + 2*k33 + k43) / 6  };
 }
 //-------------------------------------------------------------------
 // правая часть уравнения x' = f_x( t, x, y, z ) 
 //-------------------------------------------------------------------
 double f_x( double t, double x, double y, double z )
 {
-	return -y - z; 
+    return -y - z;
 }
 //-------------------------------------------------------------------
 // правая часть уравнения y' = f_y( t, x, y, z ) 
 //-------------------------------------------------------------------
 double f_y( double t, double x, double y, double z )
 {
-	return  x + a*y; 
+    return  x + a*y;
 }
 //-------------------------------------------------------------------
 // правая часть уравнения z' = f_z( t, x, y, z ) 
 //-------------------------------------------------------------------
 double f_z( double t, double x, double y, double z )
 {
-	return b + z*(x - c); 
+    return b + z*(x - c);
 }
 
